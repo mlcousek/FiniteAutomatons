eddaf1c59a91c31cd5331621fefee8a6e90a2204
Jiří Mlčoušek <jirka.mlcousek@seznam.cz>
2025-02-27 15:27:46 +0100

    Add Execute and ErrorView unit tests

---
+++ FiniteAutomatons.UnitTests/FiniteAutomatons.Core/ErrorViewTests.cs
@@ -0,0 +1,57 @@
+﻿using FiniteAutomatons.Core.Models;
+using Shouldly;
+
+namespace FiniteAutomatons.UnitTests.FiniteAutomatons.Core
+{
+    public class ErrorViewModelTests
+    {
+        [Fact]
+        public void ShowRequestId_ShouldReturnTrue_WhenRequestIdIsNotNullOrEmpty()
+        {
+            // Arrange
+            var errorViewModel = new ErrorViewModel
+            {
+                RequestId = "123"
+            };
+
+            // Act
+            var result = errorViewModel.ShowRequestId;
+
+            // Assert
+            result.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ShowRequestId_ShouldReturnFalse_WhenRequestIdIsNull()
+        {
+            // Arrange
+            var errorViewModel = new ErrorViewModel
+            {
+                RequestId = null
+            };
+
+            // Act
+            var result = errorViewModel.ShowRequestId;
+
+            // Assert
+            result.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void ShowRequestId_ShouldReturnFalse_WhenRequestIdIsEmpty()
+        {
+            // Arrange
+            var errorViewModel = new ErrorViewModel
+            {
+                RequestId = string.Empty
+            };
+
+            // Act
+            var result = errorViewModel.ShowRequestId;
+
+            // Assert
+            result.ShouldBeFalse();
+        }
+    }
+}
+
---
+++ FiniteAutomatons.UnitTests/FiniteAutomatons.Core/FiniteAutomataTests.cs
@@ -0,0 +1,109 @@
+﻿using FiniteAutomatons.Core.Models.DoMain;
+using Shouldly;
+
+namespace FiniteAutomatons.UnitTests.FiniteAutomatons.Core
+{
+    public class FiniteAutomataTests
+    {
+        [Fact]
+        public void Execute_ValidInput_ShouldReturnTrue()
+        {
+            // Arrange
+            var states = new List<State>
+            {
+                new State { Id = 1, IsStart = true, IsAccepting = false },
+                new State { Id = 2, IsStart = false, IsAccepting = true }
+            };
+
+            var transitions = new List<Transition>
+            {
+                new Transition { FromStateId = 1, ToStateId = 2, Symbol = 'a' }
+            };
+
+            var automata = new FiniteAutomata();
+            automata.States.AddRange(states);
+            automata.Transitions.AddRange(transitions);
+
+            // Act
+            var result = automata.Execute("a");
+
+            // Assert
+            result.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Execute_InvalidInput_ShouldReturnFalse()
+        {
+            // Arrange
+            var states = new List<State>
+            {
+                new State { Id = 1, IsStart = true, IsAccepting = false },
+                new State { Id = 2, IsStart = false, IsAccepting = true }
+            };
+
+            var transitions = new List<Transition>
+            {
+                new Transition { FromStateId = 1, ToStateId = 2, Symbol = 'a' }
+            };
+
+            var automata = new FiniteAutomata();
+            automata.States.AddRange(states);
+            automata.Transitions.AddRange(transitions);
+
+            // Act
+            var result = automata.Execute("b");
+
+            // Assert
+            result.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Execute_NoStartState_ShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            var states = new List<State>
+            {
+                new State { Id = 1, IsStart = false, IsAccepting = false },
+                new State { Id = 2, IsStart = false, IsAccepting = true }
+            };
+
+            var transitions = new List<Transition>
+            {
+                new Transition { FromStateId = 1, ToStateId = 2, Symbol = 'a' }
+            };
+
+            var automata = new FiniteAutomata();
+            automata.States.AddRange(states);
+            automata.Transitions.AddRange(transitions);
+
+            // Act & Assert
+            Should.Throw<InvalidOperationException>(() => automata.Execute("a"));
+        }
+
+        [Fact]
+        public void Execute_EmptyInput_ShouldReturnFalse()
+        {
+            // Arrange
+            var states = new List<State>
+            {
+                new State { Id = 1, IsStart = true, IsAccepting = false },
+                new State { Id = 2, IsStart = false, IsAccepting = true }
+            };
+
+            var transitions = new List<Transition>
+            {
+                new Transition { FromStateId = 1, ToStateId = 2, Symbol = 'a' }
+            };
+
+            var automata = new FiniteAutomata();
+            automata.States.AddRange(states);
+            automata.Transitions.AddRange(transitions);
+
+            // Act
+            var result = automata.Execute("");
+
+            // Assert
+            result.ShouldBeFalse();
+        }
+    }
+}
