a5374c1cf51cc2028a785d63c1279890580b2850
mlcousek <jirka.mlcousek@seznam.cz>
2025-03-09 10:09:49 +0100

    obsidian and new instalatoin of pc

---
+++ FiniteAutomatons.Core/Interfaces/Converters/IEpsilonNfaToNfaConverter.cs
@@ -0,0 +1,9 @@
+﻿using FiniteAutomatons.Core.Models.DoMain.FiniteAutomatons;
+
+namespace FiniteAutomatons.Core.Interfaces.Converters
+{
+    public interface IEpsilonNfaToNfaConverter
+    {
+        NFA Convert(EpsilonNFA epsilonNfa);
+    }
+}
---
+++ FiniteAutomatons.Core/Interfaces/Converters/INfaToDfaConverter.cs
@@ -0,0 +1,9 @@
+﻿using FiniteAutomatons.Core.Models.DoMain.FiniteAutomatons;
+
+namespace FiniteAutomatons.Core.Interfaces.Converters
+{
+    public interface INfaToDfaConverter
+    {
+        DFA Convert(NFA nfa);
+    }
+}
