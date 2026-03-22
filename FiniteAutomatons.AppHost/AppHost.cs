7eed1e16a0dd457c1f0344bccf1704611c989074
Jiří Mlčoušek <jmlcousek@monetplus.cz>
2026-03-22 08:34:01 +0100

    fix test plus edit text about NPDA

--- FiniteAutomatons.AppHost/AppHost.cs
+++ FiniteAutomatons.AppHost/AppHost.cs
@@ -1,4 +1,4 @@
-﻿var builder = DistributedApplication.CreateBuilder(args);
+var builder = DistributedApplication.CreateBuilder(args);
 
 var password = builder.AddParameter("password", "myStong_Password123#");
 
@@ -7,6 +7,7 @@ var db = builder.AddSqlServer("sql", password)
 
 var _ = builder.AddContainer("seq", "datalust/seq")
      .WithEnvironment("ACCEPT_EULA", "Y")
+     .WithEnvironment("SEQ_FIRSTRUN_ADMINPASSWORD", password)
      .WithHttpEndpoint(port: 5341, targetPort: 80);
 
 builder.AddProject<Projects.FiniteAutomatons>("finiteautomatons")
